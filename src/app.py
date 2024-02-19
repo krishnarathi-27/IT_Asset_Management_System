9204cb5e5064f43b7f6d01438894bf379bcf1572
krishnarathi-27 <krishna2707rathi@gmail.com>
2024-02-19 11:13:10 +0530

    added custom logger

--- src/app.py
+++ src/app.py
@@ -1,20 +1,33 @@
 import logging
+import sys
+from flask.logging import default_handler
+from logging.handlers import SysLogHandler
 from flask import Flask
 from config.app_config import AppConfig
 from config.prompts.prompts import PromptConfig
 from database.database import Database
 from config.flask_config import create_flask_config, register_blueprints, intialise_jwt_config
 
-logging.basicConfig(
-    format="%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
-    level=logging.DEBUG,
-    filename=AppConfig.LOG_LOCATION,
-)
+# logging.basicConfig(
+#     format="%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
+#     level=logging.DEBUG,
+#     filename=AppConfig.LOG_LOCATION,
+# )
 
 logger = logging.getLogger("main")
+logger.setLevel(logging.DEBUG)
 
 # def internal_server_error(e):
 #   return {"message": 'huhuhu'}, 400
+
+syslog = SysLogHandler(address=('logs2.papertrailapp.com',32836))
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s')
+
+logger.removeHandler(default_handler)
+syslog.setLevel(logging.DEBUG)
+syslog.setFormatter(formatter)
+
+logger.addHandler(syslog)
 
 def create_app():
     """Creating flask app server and initialising all configs and database tables """
