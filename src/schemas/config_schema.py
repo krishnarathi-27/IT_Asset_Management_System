d5c76d8613ef310bc8e46c70ef9d3778fa59c32b
krishnarathi-27 <krishna2707rathi@gmail.com>
2024-02-20 13:54:07 +0530

    updated error responses

--- src/schemas/config_schema.py
+++ src/schemas/config_schema.py
@@ -5,4 +5,4 @@ from src.utils.exceptions import ApplicationException
 class MySchema(Schema):
 
     def handle_error(self, error: fields.ValidationError, data: Any, *, many: bool, **kwargs):
-        raise ApplicationException(422,"invalid request","huhuhuh")
+        raise ApplicationException(422,"Unprocessible Entity","Request body have invalid fields")
